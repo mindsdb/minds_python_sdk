5fc72a4a6123cbce0e59f135552729e9559d74c9
Zoran Pandovski <zoran.pandovski@gmail.com>
2024-09-13 12:52:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+about = {}
+with open("minds/__about__.py") as fp:
+    exec(fp.read(), about)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as req_file:
+    requirements = req_file.read().splitlines()
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    url=about['__github__'],
+    download_url=about['__pypi__'],
+    author=about['__author__'],
+    author_email=about['__email__'],
+    description=about['__description__'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=('tests*',)),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
