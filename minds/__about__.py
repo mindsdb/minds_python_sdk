023b2f3a7a6ec1afeceb6fb2654ab39063444b98
Zoran Pandovski <zoran.pandovski@gmail.com>
2024-09-17 18:06:45 +0200

    Update __about__.py

--- minds/__about__.py
+++ minds/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'minds_sdk'
 __package_name__ = 'minds'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __description__ = 'An AI-Data Mind is an LLM with the built-in power to answer data questions for Agents'
 __email__ = 'hello@mindsdb.com'
 __author__ = 'MindsDB Inc'
